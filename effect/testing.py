c15fe77c782d05d254eb6b9ccbe8c28cdb517924
Tom Prince <tom.prince@ualberta.net>
2014-12-21 14:17:50 -0700

    Replace effect.testing with canned dispatcher.

--- effect/testing.py
+++ effect/testing.py
@@ -4,120 +4,24 @@ Various functions for inspecting and restructuring effects.
 
 from __future__ import print_function
 
-import sys
 
-from characteristic import attributes
+class CannedDispatcher(object):
 
-from . import Effect, guard, ParallelEffects
+    def __init__(self):
+        self._expected = []
 
-import six
+    def add_success(self, intent, result):
+        self._expected.append((intent, False, result))
 
+    def add_failure(self, intent, result):
+        self._expected.append((intent, True, result))
 
-@attributes(['intent'], apply_with_init=False)
-class StubIntent(object):
-    """
-    An intent which wraps another intent, to flag that the intent should
-    be automatically resolved by :func:`resolve_stub`.
-
-    This intent is intentionally not performable by any default mechanism.
-    """
-
-    def __init__(self, intent):
-        self.intent = intent
-
-
-def resolve_effect(effect, result, is_error=False):
-    """
-    Supply a result for an effect, allowing its callbacks to run.
-
-    The return value of the last callback is returned, unless any callback
-    returns another Effect, in which case an Effect representing that
-    operation plus the remaining callbacks will be returned.
-
-    This allows you to test your code in a somewhat "channel"-oriented
-    way:
-
-        eff = do_thing()
-        next_eff = resolve_effect(eff, first_result)
-        next_eff = resolve_effect(next_eff, second_result)
-        result = resolve_effect(next_eff, third_result)
-
-    Equivalently, if you don't care about intermediate results:
-
-        result = resolve_effect(
-            resolve_effect(
-                resolve_effect(
-                    do_thing(),
-                    first_result),
-                second_result),
-            third_result)
-
-    NOTE: parallel effects have no special support. They can be resolved with
-    a sequence, and if they're returned from another effect's callback they
-    will be returned just like any other effect.
-    """
-    for i, (callback, errback) in enumerate(effect.callbacks):
-        cb = errback if is_error else callback
-        if cb is None:
-            continue
-        is_error, result = guard(cb, result)
-        if type(result) is Effect:
-            return Effect(
-                result.intent,
-                callbacks=result.callbacks + effect.callbacks[i + 1:])
-    if is_error:
-        six.reraise(*result)
-    return result
-
-
-def fail_effect(effect, exception):
-    """
-    Resolve an effect with an exception, so its error handler will be run.
-    """
-    try:
-        raise exception
-    except:
-        return resolve_effect(effect, sys.exc_info(), is_error=True)
-
-
-def resolve_stub(effect):
-    """
-    Automatically perform an effect, if its intent is a StubIntent.
-
-    Note that resolve_stubs is preferred to this function, since it handles
-    chains of stub effects.
-    """
-    if type(effect.intent) is StubIntent:
-        is_error, result = guard(effect.intent.intent.perform_effect, None)
-        return resolve_effect(effect, result, is_error=is_error)
-    else:
-        raise TypeError("resolve_stub can only resolve stubs, not %r"
-                        % (effect,))
-
-
-def resolve_stubs(effect):
-    """
-    Successively performs effects with resolve_stub until a non-Effect value,
-    or an Effect with a non-stub intent is returned, and return that value.
-
-    Parallel effects are supported by recursively invoking resolve_stubs on
-    the child effects, if all of their children are stubs.
-    """
-    if type(effect) is not Effect:
-        raise TypeError("effect must be Effect: %r" % (effect,))
-
-    while type(effect) is Effect:
-        if type(effect.intent) is StubIntent:
-            effect = resolve_stub(effect)
-        elif type(effect.intent) is ParallelEffects:
-            if not all(isinstance(x.intent, StubIntent)
-                       for x in effect.intent.effects):
-                break
+    def __call__(self, dispatcher, intent, box):
+        expected, is_error, result = self._expected.pop(0)
+        if intent == expected:
+            if is_error:
+                box.fail(result)
             else:
-                effect = resolve_effect(
-                    effect,
-                    list(map(resolve_stubs, effect.intent.effects)))
+                box.succeed(result)
         else:
-            break
-
-    return effect
+            raise AssertionError("Unexecpected intent.")
